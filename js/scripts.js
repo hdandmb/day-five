26c95b7b51e6f8c60340e949ad999f29d50b2c2b
Rocky Garcia <purplyrocky@gmail.com>
2017-09-19 15:48:19 -0700

    added all files

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+// Link this js file using the "external" method we learned today
+
+// Which keyword is used to create a variable? Give an example of a camel cased variable
+
+// Use the console to log out the title of your favorite movie
+
+// In a multi-line comment below, list 3 favorite foods
+
+// Create an alert
+
+// Ask the user to confirm something and console log their response
+
+// Ask the user a question and console log their response
